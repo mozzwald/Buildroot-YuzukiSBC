3341722c9279445f8dc0c724d773d11b5da665c8
mozzwald <mozzwald@gmail.com>
2025-04-14 11:24:29 -0500

    atari800: optimize build for lc-pi

--- buildroot/package/atari800/atari800.mk
+++ buildroot/package/atari800/atari800.mk
@@ -4,18 +4,21 @@ ATARI800_SITE_METHOD = git
 ATARI800_NO_CHECK_HASH_FOR = ATARI800
 
 ATARI800_AUTORECONF = NO
-ATARI800_DEPENDENCIES = host-pkgconf sdl host-autoconf host-automake host-libtool host-m4
+ATARI800_DEPENDENCIES = sdl libpng host-pkgconf host-autoconf host-automake host-libtool host-m4
 
 define ATARI800_CONFIGURE_CMDS
     cd $(@D) && \
     PATH="$(HOST_DIR)/bin:$$PATH" ./autogen.sh && \
     PATH="$(HOST_DIR)/bin:$$PATH" \
-    SDL_CONFIG="$(STAGING_DIR)/usr/bin/sdl-config" \
     ./configure \
+        ac_cv_c_bigendian=no  \
         --prefix=/usr \
         --host=$(GNU_TARGET_NAME) \
         --build=$(GNU_HOST_NAME) \
-        --with-sdl \
+        --with-video=sdl \
+        --with-sound=sdl \
+        --without-opengl \
+        SDL_CONFIG=$(STAGING_DIR)/usr/bin/sdl-config \
         CC="$(TARGET_CC)" \
         CPP="$(TARGET_CPP)" \
         CXX="$(TARGET_CXX)" \
@@ -26,7 +29,8 @@ define ATARI800_CONFIGURE_CMDS
         PKG_CONFIG="$(PKG_CONFIG_HOST_BINARY)" \
         PKG_CONFIG_SYSROOT_DIR="$(STAGING_DIR)" \
         PKG_CONFIG_LIBDIR="$(STAGING_DIR)/usr/lib/pkgconfig:$(STAGING_DIR)/usr/share/pkgconfig" \
-        CFLAGS="--sysroot=$(STAGING_DIR)" \
+        CFLAGS="-O3 -mcpu=cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=hard \
+            -ftree-vectorize -flto --sysroot=$(STAGING_DIR)" \
         CPPFLAGS="--sysroot=$(STAGING_DIR)" \
         LDFLAGS="--sysroot=$(STAGING_DIR)"
 endef
